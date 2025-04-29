20afac37527071be3c7e70b5209598950b4a2ba8
Hossam Shafeek <hossamshafik55@gmail.com>
2025-04-29 16:18:58 +0300

    first commit

---
+++ 1_to_8_variable.py
@@ -0,0 +1,39 @@
+
+print('I Love Python') 
+
+print(type(10.0)) # => float = Floating Point Number
+
+print(type(10)) # => int = Integer Number
+
+print(type(10==10)) # => bool = Boolean
+
+print(type('Hossam')) # => str = String
+
+print(type([10,20,30,40,50])) # => list =List
+
+print(type((10,20,30,40,50))) # => tuple = Tuple
+
+print(type({'name':'Hossam','age':25,'address':'Giza'})) # => dict = Dictionary
+
+# ----- Variables -----
+# Syntax => Variable Name - Assignment Operator - Value
+
+# Convention and Rules
+# 1- Can Start With (a-z A-Z) Or Underscore
+# 2- Can Not Start With Number Or Special Characters
+# 3- 
+#
+#
+
+name  ='Hossam' # Single Word => Normal
+name_two  ='Hossam' # Tow Word => CamelCase
+nameTwo  ='Hossam' # Tow Word => SnakCase
+
+print(name)
+print(name_two)
+print(nameTwo)
+
+# Reserved Word
+help('keywords')
+
+a,b,c =1,2,3
